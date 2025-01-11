acd360de7cc48796bb0aa5a0b66b69841b8a6279
Valentyna1302 <122233669+Valentyna1302@users.noreply.github.com>
2025-01-11 13:50:47 +0100

    update profile

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,6 @@
 import Profile from "./components/Profile/Profile";
 
-const userData = {
-  username: "Jacques Gluke",
-  tag: "jgluke",
-  location: "Ocho Rios, Jamaica",
-  avatar: "https://cdn-icons-png.flaticon.com/512/2922/2922506.png",
-  stats: {
-    followers: 5603,
-    views: 4827,
-    likes: 1308,
-  },
-};
+import userData from "./userData.json";
 
 export default function App() {
   return (
