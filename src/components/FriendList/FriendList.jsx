de5b09baebcc878b8df153022a97c2520a269384
Valentyna1302 <122233669+Valentyna1302@users.noreply.github.com>
2025-01-11 15:28:24 +0100

    update friendlist

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,27 @@
+export default function FriendList({ friends }) {
+  return (
+    <ul>
+      {friends.map((friend) => {
+        return (
+          <li key={friend.id}>
+            <FriendListItem
+              name={friend.name}
+              avatar={friend.avatar}
+              isOnline={friend.isOnline}
+            />
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
+
+function FriendListItem({ avatar, name, isOnline }) {
+  return (
+    <div>
+      <img src={avatar} alt="Avatar" width="48" />
+      <p>{name}</p>
+      <p>{isOnline ? "Online" : "Offline"}</p>
+    </div>
+  );
+}
